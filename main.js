e0454f0723cc3761d3c313193a2c90f06adebda6
tvvmvn <tvvmvn@gmail.com>
2024-10-01 14:38:32 +0900

    10/1

--- main.js
+++ main.js
@@ -1,78 +1,108 @@
-// canvas
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
-canvas.width = innerWidth;
-canvas.height = innerHeight;
-canvas.style.backgroundColor = "#222";
+(function () {
+  // canvas
+  var canvas = document.getElementById("canvas");
+  var ctx = canvas.getContext("2d");
+  canvas.width = innerWidth;
+  canvas.height = innerHeight;
+  canvas.style.backgroundColor = "#222";
+  canvas.addEventListener("click", clickHandler);
 
-// controller
-canvas.addEventListener("click", function () {
-  var r = Math.floor((e.offsetY - GRID_OFFSET_Y) / GRID_ITEM_SIZE);
-  var c = Math.floor((e.offsetX - GRID_OFFSET_X) / GRID_ITEM_SIZE);
+  // constants 
+  const GRID_SIZE = 300;
+  const GRID_OFFSET_X = (canvas.width - GRID_SIZE) / 2;
+  const GRID_OFFSET_Y = 100;
+  const GRID_LINE_COUNT = 3;
+  const GRID_ITEM_SIZE = GRID_SIZE / GRID_LINE_COUNT;
+  const USER = 1;
+  const COM = 2;
 
-  if (r > -1 && r < GRID_LINE_COUNT && c > -1 && c < GRID_LINE_COUNT) {
-    inputRow = r;
-    inputCol = c;
+  // variables 
+  var lot = Math.ceil(Math.random() * 2);
+  var turn = lot;
+  var bingo;
+  var drawn;
+  var row, col;
+  var count = 0;
+  var board = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0],
+  ]
+
+  // control 
+  function clickHandler(e) {
+    if (turn == USER) {
+      var r = Math.floor((e.offsetY - GRID_OFFSET_Y) / GRID_ITEM_SIZE);
+      var c = Math.floor((e.offsetX - GRID_OFFSET_X) / GRID_ITEM_SIZE);
+  
+      if (r > -1 && r < GRID_LINE_COUNT && c > -1 && c < GRID_LINE_COUNT) {
+        row = r;
+        col = c;
+  
+        if (board[row][col] == 0) {
+          board[row][col] = USER;
+          turn = COM;
+          count++;
+        }
+      }
+    }
   }
-});
 
-// input
-var umpire = new Umpire();
-var user = new User();
-var com = new Com();
-var stage = new Stage();
-var board = [
-  [0, 0, 0],
-  [0, 0, 0],
-  [0, 0, 0],
-];
+  // run the game 
+  function main() {
+    clearCanvas();
+    drawBoard();
 
-// root.render()
-function main() {
-  clearCanvas();
-  stage.render();
-  user.render();
-  com.render();
-  umpire.render();
-}
-
-// Class
-class Stage {
-  GRID_SIZE = 300;
-  GRID_OFFSET_X = (canvas.width - GRID_SIZE) / 2;
-  GRID_OFFSET_Y = 100;
-  GRID_LINE_COUNT = 3;
-  GRID_ITEM_SIZE = GRID_SIZE / GRID_LINE_COUNT;
-
-  drawBoard() {
-    ctx.beginPath();
-    ctx.strokeStyle = "#444";
-    ctx.lineWidth = 4;
-
-    for (var r = 1; r < GRID_LINE_COUNT; r++) {
-      ctx.moveTo(GRID_OFFSET_X, GRID_OFFSET_Y + (GRID_ITEM_SIZE * r));
-      ctx.lineTo(GRID_OFFSET_X + GRID_SIZE, GRID_OFFSET_Y + (GRID_ITEM_SIZE * r));
+    if (count < 2) {
+      if (lot == USER) {
+        drawMessage("YOU FIRST");
+      } else {
+        drawMessage("COM FIRST");
+      }
     }
 
-    for (var c = 1; c < GRID_LINE_COUNT; c++) {
-      ctx.moveTo(GRID_OFFSET_X + (GRID_ITEM_SIZE * c), GRID_OFFSET_Y);
-      ctx.lineTo(GRID_OFFSET_X + (GRID_ITEM_SIZE * c), GRID_OFFSET_Y + GRID_SIZE);
+    bingo = isBingo();
+    drawn = isDrawn();
+
+    if (bingo || drawn) {
+      if (bingo) {
+        if (bingo == USER) {
+          drawMessage("YOU WIN!");
+        } else {
+          drawMessage("YOU LOSE!");
+        }
+      } else {
+        drawMessage("DRAW!");
+      }
+    } else {
+      if (turn == COM) {
+        setTimeout(com, 1000);
+        turn = null;
+      }
+
+      requestAnimationFrame(main);
+    }
+  };
+
+  main();
+
+  // functions 
+  function com() {
+    while (true) {
+      var r = Math.floor(Math.random() * 3);
+      var c = Math.floor(Math.random() * 3);
+
+      if (board[r][c] == 0) {
+        board[r][c] = COM;
+        count++;
+        break;
+      }
     }
 
-    ctx.stroke();
-  }
-}
-
-class Umpire {
-  // 0 drawn, 1 user, 2 com
-  result = -1; 
-
-  // who do first 
-  lot() {
-    return Math.ceil(Math.random() * 2);
+    turn = USER;
   }
 
-  isBingo() {
+  function isBingo() {
     // horizontal bingo
     for (var r = 0; r < 3; r++) {
       if (
@@ -117,84 +147,69 @@ class Umpire {
     return false;
   }
 
-  isDrawn() {
-    for (var r = 0; r < 3; r++) {
-      for (var c = 0; c < 3; c++) {
-        if (board[r][c] == 0) {
-          return false;
+  function isDrawn() {
+    return count == 9;
+  }
+
+  // draw 
+  function clearCanvas() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  }
+
+  function drawBoard() {
+    // board
+    ctx.beginPath();
+    ctx.strokeStyle = "#444";
+    ctx.lineWidth = 4;
+
+    for (var r = 1; r < GRID_LINE_COUNT; r++) {
+      ctx.moveTo(GRID_OFFSET_X, GRID_OFFSET_Y + (GRID_ITEM_SIZE * r));
+      ctx.lineTo(GRID_OFFSET_X + GRID_SIZE, GRID_OFFSET_Y + (GRID_ITEM_SIZE * r));
+    }
+
+    for (var c = 1; c < GRID_LINE_COUNT; c++) {
+      ctx.moveTo(GRID_OFFSET_X + (GRID_ITEM_SIZE * c), GRID_OFFSET_Y);
+      ctx.lineTo(GRID_OFFSET_X + (GRID_ITEM_SIZE * c), GRID_OFFSET_Y + GRID_SIZE);
+    }
+
+    ctx.stroke();
+
+    // symbols
+    for (var r = 0; r < board.length; r++) {
+      for (var c = 0; c < board[r].length; c++) {
+        var id = board[r][c];
+
+        var x = GRID_OFFSET_X + (c * GRID_ITEM_SIZE);
+        var y = GRID_OFFSET_Y + (r * GRID_ITEM_SIZE);
+
+        ctx.beginPath();
+        ctx.strokeStyle = "#fff";
+        ctx.lineWidth = 8;
+
+        if (id == 1) {
+          ctx.arc(x + 50, y + 50, 30, 0, 2 * Math.PI);
+          ctx.stroke();
+        } 
+
+        if (id == 2) {
+          ctx.moveTo(x + 20, y + 20);
+          ctx.lineTo(x + 80, y + 80);
+          ctx.moveTo(x + 80, y + 20);
+          ctx.lineTo(x + 20, y + 80);
         }
-      }
-    }
-
-    return true;
-  }
-
-  render() {
-    if (over) {
-      // message: YOU WIN! or YOU LOSE
-    } else if (drawn) {
-      // message: DRAW!
-    }
-  }
-}
-
-class User {
-  inputRow = 0;
-  inputCol = 0;
-
-  render() {
-    for (var r = 0; r < board.length; r++) {
-      for (var c = 0; c < board[r].length; c++) {
-        var x = GRID_OFFSET_X + (c * GRID_ITEM_SIZE);
-        var y = GRID_OFFSET_Y + (r * GRID_ITEM_SIZE);
-
-        ctx.beginPath();
-        ctx.strokeStyle = "#fff";
-        ctx.lineWidth = 8;
-
-        ctx.arc(x + 50, y + 50, 30, 0, 2 * Math.PI);
-        ctx.stroke();
-      }
-    }
-  }
-}
-
-class Com {
-  row;
-  col;
-
-  choose() {
-    while (true) {
-      var r = Math.floor(Math.random() * 3);
-      var c = Math.floor(Math.random() * 3);
-
-      if (board[r][c] == 0) {
-        board[r][c] = COM;
-        break;
-      }
-    }
-
-    turn = USER;
-  }
-
-  render() {
-    for (var r = 0; r < board.length; r++) {
-      for (var c = 0; c < board[r].length; c++) {
-        var x = GRID_OFFSET_X + (c * GRID_ITEM_SIZE);
-        var y = GRID_OFFSET_Y + (r * GRID_ITEM_SIZE);
-
-        ctx.beginPath();
-        ctx.strokeStyle = "#fff";
-        ctx.lineWidth = 8;
-
-        ctx.moveTo(x + 20, y + 20);
-        ctx.lineTo(x + 80, y + 80);
-        ctx.moveTo(x + 80, y + 20);
-        ctx.lineTo(x + 20, y + 80);
 
         ctx.stroke();
       }
-    }    
+    }
   }
-}
+
+  function drawMessage(message) {
+    ctx.font = "20px Monospace";
+    ctx.fillStyle = "#fff";
+    ctx.textAlign = "center";
+    ctx.fillText(message, canvas.width / 2, 60);
+  }
+})();
+
+
 
