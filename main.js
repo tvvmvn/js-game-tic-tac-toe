eed458905521a79e952225304a74b4c0954cc7f1
tvvmvn <tvvmvn@gmail.com>
2024-10-18 14:43:48 +0900

    10.18

--- main.js
+++ main.js
@@ -16,15 +16,24 @@ class Game {
   USER = 1;
   COM = 2;
   // variables 
-  first = Math.ceil(Math.random() * 2);
-  turn = this.first;
-  count = 0;
+  first = this.lot();
+  whoseTurn = this.first;
+  gameCount = 0;
   message = "";
   board = [
     [0, 0, 0],
     [0, 0, 0],
     [0, 0, 0],
-  ]
+  ];
+  timer;
+
+  constructor() {
+    this.timer = setInterval(() => this.actionPerformed(), 10);
+  }
+
+  lot() {
+    return Math.ceil(Math.random() * 2);
+  }
 
   getCrds() {
     while (true) {
@@ -36,56 +45,61 @@ class Game {
       }
     }
   }
-  
-  isBingo() {
-    // horizontal bingo
+
+  rowCrossed() {
     for (var r = 0; r < 3; r++) {
       if (
         this.board[r][0] != 0
         && this.board[r][0] == this.board[r][1] 
         && this.board[r][1] == this.board[r][2]
       ) {
-        return this.board[r][0];
+        return true;
       }
     }
-    
-    // vertical bingo
+
+    return false;
+  }
+
+  columnCrossed() {
     for (var c = 0; c < 3; c++) {
       if (
         this.board[0][c] != 0
         && this.board[0][c] == this.board[1][c] 
         && this.board[1][c] == this.board[2][c]
       ) {
-        return this.board[0][c];
+        return true;
       }
-    }
-  
-    // cross bingo (\)
-    if (
-      this.board[0][0] != 0
-      && this.board[0][0] == this.board[1][1] 
-      && this.board[1][1] == this.board[2][2]
-    ) {
-      return this.board[0][0];
-    }
-  
-    // cross bingo (/)
-    if (
-      this.board[0][2] != 0
-      && this.board[0][2] == this.board[1][1] 
-      && this.board[1][1] == this.board[2][0]
-    ) {
-      return this.board[0][2];
     }
 
     return false;
   }
-  
-  isDrawn() {
-    if (this.count == 9) {
+
+  diagonalCrossed() {
+    // crossed like \
+    if ( 
+      this.board[0][0] != 0
+      && this.board[0][0] == this.board[1][1] 
+      && this.board[1][1] == this.board[2][2]
+    ) { 
+      return true;
+    
+    // crossed like /
+    } else if ( 
+      this.board[0][2] != 0
+      && this.board[0][2] == this.board[1][1] 
+      && this.board[1][1] == this.board[2][0]
+    ) {
       return true;
     }
-
+  
+    return false;
+  }
+  
+  isCrossed() {
+    if (this.rowCrossed() || this.columnCrossed() || this.diagonalCrossed()) {
+      return true;
+    }
+    
     return false;
   }
 
@@ -122,9 +136,7 @@ class Game {
         if (id == 1) {
           ctx.arc(x + 50, y + 50, 30, 0, 2 * Math.PI);
           ctx.stroke();
-        } 
-
-        if (id == 2) {
+        } else if (id == 2) {
           ctx.moveTo(x + 20, y + 20);
           ctx.lineTo(x + 80, y + 80);
           ctx.moveTo(x + 80, y + 20);
@@ -147,10 +159,11 @@ class Game {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
   }
 
-  render() {
+  actionPerformed() {
     this.clearCanvas();
 
-    if (this.count < 2) {
+    // Start message
+    if (this.gameCount < 2) {
       if (this.first == this.USER) {
         this.drawMessage("YOU FIRST");
       } else {
@@ -162,45 +175,44 @@ class Game {
 
     this.drawBoard();
     this.drawSymbol();
-
-    var bingo = this.isBingo();
-    var drawn = this.isDrawn();
   
-    if (bingo) {
-      if (bingo == this.USER) {
-        this.drawMessage("YOU WIN!");
-      } else {
-        this.drawMessage("YOU LOSE");
-      }
-    } else if (drawn) {
-      this.drawMessage("DRAW!");
-    } else {
-      if (this.turn == this.COM) {
+    if (!this.isCrossed() && this.gameCount < 9) {
+      if (this.whoseTurn == this.COM) {
         setTimeout(() => {
           var [r, c] = this.getCrds();
+          
           this.board[r][c] = this.COM;
-
-          this.turn = this.USER;
-          this.count++;
+          this.whoseTurn = this.USER;
+          this.gameCount++;
         }, 1000);
 
-        this.turn = null;
+        this.whoseTurn = null;
+      }
+    } else {
+      if (this.isCrossed()) {
+        if (this.whoseTurn == this.COM) {
+          this.drawMessage("YOU WIN!");
+        } else {
+          this.drawMessage("YOU LOSE");
+        }
+      } else {
+        this.drawMessage("DRAW");
       }
 
-      requestAnimationFrame(() => this.render());
+      clearInterval(this.timer);
     }
   }
 
   clickHandler(e) {
-    if (this.turn == this.USER) {
+    if (this.whoseTurn == this.USER) {
       var r = Math.floor((e.offsetY - this.GRID_OFFSET_Y) / this.GRID_ITEM_SIZE);
       var c = Math.floor((e.offsetX - this.GRID_OFFSET_X) / this.GRID_ITEM_SIZE);
   
       if (r > -1 && r < this.GRID_LINE_COUNT && c > -1 && c < this.GRID_LINE_COUNT) {
         if (this.board[r][c] == 0) {
           this.board[r][c] = this.USER;
-          this.turn = this.COM;
-          this.count++;
+          this.whoseTurn = this.COM;
+          this.gameCount++;
         }
       }
     }
@@ -209,5 +221,3 @@ class Game {
 
 var game = new Game();
 canvas.addEventListener("click", (e) => game.clickHandler(e));
-
-game.render();
